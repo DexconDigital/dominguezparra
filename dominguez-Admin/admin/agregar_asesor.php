9c2d1e29e082e7e10fc71f6d6276f4bbe0139028
william solano <wdsp9898@gmail.com>
2020-02-26 09:26:08 -0500

    Se realizan validaciones del admin

--- dominguez-Admin/admin/agregar_asesor.php
+++ dominguez-Admin/admin/agregar_asesor.php
@@ -4,7 +4,6 @@ require_once 'conexion.php';
 
 $nom_asesor = $_REQUEST["nom_asesor"];
 $cel_asesor = $_REQUEST["cel_asesor"];
-$tel_asesor = $_REQUEST["tel_asesor"];
 $cor_asesor = $_POST["cor_asesor"];
 $image = $_FILES["imagen"]["name"];
 $ruta = $_FILES["imagen"]["tmp_name"];
@@ -14,13 +13,14 @@ $fecha = $_REQUEST["fecha"];
 $fecha = date("Y-m-d");
 $cargo = $_REQUEST["cargo"];
 // $descrip = $_REQUEST["descrip"];
+$tel_asesor = $_REQUEST["tel_asesor"];
 $id_inmo = 15;
-
-//  echo $nom_asesor, '', $cel_asesor, '',$tel_asesor,'', $cor_asesor ,'',$image ,'',$ruta ,'', $nombre_foto ,'', $destino ,'',$fecha, '',$cargo, '',$descrip;
-// die();
+//  echo $nom_asesor, '', $cel_asesor, '',$tel_asesor,'', $cor_asesor ,'',$image ,'',$ruta ,'', $nombre_foto ,'', $destino ,'',$fecha, '',$cargo, '',$id_inmo;
+//  die();
 $con = Conect();
 if (isset($destino)) {
-    mysqli_query($con, "INSERT INTO `asesores` (`id`, `nombre`,`telefono`,`correo`,`imagen`,`id_inmobiliaria`,`fecha`,`cargo`,`celular`) VALUES (NULL, '$nom_asesor','$tel_asesor','$cor_asesor','$destino','$id_inmo','$fecha','$cargo','$cel_asesor')");
+    // mysqli_query($con, "INSERT INTO `asesores` (`id`, `nombre`,`telefono`,`correo`,`imagen`,`id_inmobiliaria`,`fecha`,`cargo`,`celular`) VALUES (NULL, '$nom_asesor','$tel_asesor','$cor_asesor','$destino','$id_inmo','$fecha','$cargo','$cel_asesor')");
+    mysqli_query($con, "INSERT INTO `asesores` (`id`, `nombre`, `telefono`, `correo`, `imagen`, `id_inmobiliaria`, `fecha`, `cargo`, `descripcion`, `celular`) VALUES (NULL, '$nom_asesor', '$cel_asesor', '$cor_asesor', '$destino', '$id_inmo', '$fecha', '$cargo', '', '$cel_asesor');");
     echo  "<script language='javascript'>
 alert('Asesor Agregado Con Éxito');
 window.location.href='index.php'
--- dominguez-Admin/admin/update_asesor.php
+++ dominguez-Admin/admin/update_asesor.php
@@ -5,7 +5,6 @@ require_once ('conexion.php');
 $id=$_REQUEST["id"];
 $nom_asesor=$_REQUEST["nom_asesor"];
 $cel_asesor=$_REQUEST["cel_asesor"];
-$wapp_asesor=$_REQUEST["wapp_asesor"];
 $cor_asesor= $_POST["cor_asesor"];
 $image=$_FILES["imagen"]["name"];
 $ruta=$_FILES["imagen"]["tmp_name"];
@@ -13,38 +12,38 @@ $nombre_foto = str_replace(" ","",$image);
 $destino="asesores/".$nombre_foto;
 $comparador_fotos="asesores/";
 $cargo=$_REQUEST["cargo"];
-$descrip=$_REQUEST ["descrip"];
+$wapp_asesor=$_REQUEST["wapp_asesor"];
+// $descrip=$_REQUEST ["descrip"];
 
-// No actualizar ni archivos ni imagenes
+// No actualizar ni archivos ni noticias
 if($destino == $comparador_fotos){
     $con1 = Conect();
      $qry1="SELECT * FROM asesores where id ='$id'";
             $sql1=mysqli_query($con1,$qry1);
             $res=  mysqli_fetch_array($sql1) ;
             
-            // $destino = $res[4];
+            $destino = $res[4];
     $con = Conect();
-    $qry=("update asesores set nombre='$nom_asesor', telefono='$cel_asesor', correo='$cor_asesor', cargo='$cargo' , descripcion='$descrip', celular='$wapp_asesor' where id='$id '");
+    $qry=("update asesores set nombre='$nom_asesor', telefono='$cel_asesor', correo='$cor_asesor', cargo='$cargo', celular='$wapp_asesor' where id='$id '");
     $sql=mysqli_query($con,$qry);
         if(!$sql){
+        echo 'No se logro actualizar';
     }else{
-        echo  "<script language='javascript'>
+       echo  "<script language='javascript'>
         alert('Asesor Modificado con Éxito');
          window.location.href='index.php'
       </script>";
     }
+    // actualizar fotos pero no archivo
 }
 if($destino != $comparador_fotos){
-    $con1 = Conect();
-     $qry1="SELECT * FROM asesores where id ='$id'";
-            $sql1=mysqli_query($con1,$qry1);
-            $res=  mysqli_fetch_array($sql1) ;
-            
-            // $destino = $res[4];
+    copy($ruta,$destino);
     $con = Conect();
-    $qry=("update asesores set nombre='$nom_asesor', telefono='$cel_asesor', correo='$cor_asesor', cargo='$cargo' , descripcion='$descrip', celular='$wapp_asesor',imagen='$destino' where id='$id '");
-    $sql=mysqli_query($con,$qry);
-        if(!$sql){
+    $qry=("update asesores set nombre='$nom_asesor', telefono='$cel_asesor', correo='$cor_asesor', imagen='$destino', cargo='$cargo', celular='$wapp_asesor' where id='$id '");
+    $sql=mysqli_query($con,$qry);  
+
+    if(!$sql){
+        echo 'No se logro actualizar';
     }else{
         echo  "<script language='javascript'>
         alert('Asesor Modificado con Éxito');
@@ -52,7 +51,6 @@ if($destino != $comparador_fotos){
       </script>";
     }
 }
-
 
 
 ?>
