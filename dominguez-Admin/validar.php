43e7401ca4d5a2bcae9cefa88a6d0d96d03c272c
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-06-18 16:01:43 -0500

    pdo

--- dominguez-Admin/validar.php
+++ dominguez-Admin/validar.php
@@ -1,16 +1,23 @@
-<?php 
-require './conexion2.php';
+<?php
+require 'admin/conexion.php';
+$con = Conect();
 $id_inmobiliaria = 15;
-sleep(2);
-$usuarios = $mysqli->query("SELECT id_user
+
+$qry = ( "SELECT id_user
 FROM usuarios WHERE id_user = '$id_inmobiliaria' AND usuario = '".$_POST['usuariolg']."' 
-AND password = '".$_POST['passlg']."'");
-if($usuarios->num_rows == 1):
-    $datos = $usuarios->fetch_assoc();
-    echo json_encode(array('error' => false, 'tipo' => $datos['id_user']));
+AND password = '".$_POST['passlg']."'" );
+
+$result = $con->prepare( $qry );
+$result->execute();
+
+$row = $result->fetch();
+$rowcount = $result->rowCount();
+
+if ( $rowcount == 1 ):
+$datos = $row;
+echo json_encode( array( 'error' => false, 'tipo' => $datos['id_user'] ) );
 else:
-    echo json_encode(array('error' => true));
+echo json_encode( array( 'error' => true ) );
 endif;
-$mysqli->close();
 ?>
 
