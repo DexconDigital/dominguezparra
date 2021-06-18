8547021d93ab94a9a279a3fdcf06f82751896ea8
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-06-18 16:02:02 -0500

    Delete dominguez-admin directory

--- dominguez-admin/conexion2.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost','root','','dexcondigital_noticias_inmueble');
-if($mysqli->connect_errno):
-    echo "Error al conectarse con Mysql debiado al error" , $mysqli->connect_errno;
-endif;
-?>
